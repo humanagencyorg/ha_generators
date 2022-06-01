8d7b1100bbfb7c17ca081a5ea6b0f4d9c06b9dea
Oleksandr Veshcherovych <oleksandr@humanagency.com>
2022-06-01 19:54:45 +0300

    Remove abstract_generator from generators

--- lib/generators/ha/abstract_generator.rb
+++ lib/generators/ha/abstract_generator.rb
@@ -1,29 +1,25 @@
 require "rails/generators"
 
-module Ha
-  module Generators
-    class AbstractGenerator < Rails::Generators::NamedBase
-      def generate_file
-        filepath = build_filepath("app/#{@type_name}s", class_name, @suffix)
-        dir = File.dirname(filepath)
-        FileUtils.mkdir_p(dir) unless File.exist?(dir)
+class AbstractGenerator < Rails::Generators::NamedBase
+  def generate_file
+    filepath = build_filepath("app/#{@type_name}s", class_name, @suffix)
+    dir = File.dirname(filepath)
+    FileUtils.mkdir_p(dir) unless File.exist?(dir)
 
-        template "#{@type_name}.erb", filepath
-      end
+    template "#{@type_name}.erb", filepath
+  end
 
-      def generate_spec_file
-        filepath = build_filepath("spec/#{@type_name}s", class_name, @spec_suffix)
-        dir = File.dirname(filepath)
-        FileUtils.mkdir_p(dir) unless File.exist?(dir)
+  def generate_spec_file
+    filepath = build_filepath("spec/#{@type_name}s", class_name, @spec_suffix)
+    dir = File.dirname(filepath)
+    FileUtils.mkdir_p(dir) unless File.exist?(dir)
 
-        template "#{@type_name}_spec.erb", filepath
-      end
+    template "#{@type_name}_spec.erb", filepath
+  end
 
-      private
+  private
 
-      def build_filepath(folder_name, class_name, suffix)
-        "#{folder_name}/#{class_name.underscore}#{suffix}"
-      end
-    end
+  def build_filepath(folder_name, class_name, suffix)
+    "#{folder_name}/#{class_name.underscore}#{suffix}"
   end
 end
